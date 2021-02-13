6f3141f20fa0aecb4d4b4f8db1ccf652958523d7
abr-ya <gamer05@list.ru>
2021-02-13 12:51:29 +0300

    feat: towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 
 // You should implement your task here.
+module.exports = function towelSort (matrix) { // закомментить для проверки через npm start
+// const towelSort = (matrix) => { // для проверки через npm start
+  if (matrix === undefined) return []; 
+  // console.log(matrix);
+  // console.log(matrix.length);
 
-module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    const forward = i % 2 === 0;
+    // console.log(row, forward);
+    while (row.length > 0) {
+      result.push(forward ? row.shift() : row.pop());
+    }
+  }
+
+  return result;
 }
+
+// для проверки через npm start
+// const matrix1 = [
+//   [1, 2, 4],
+//   [5, 6, 7, 8],
+//   [9, 12],
+// ];
+
+// console.log(towelSort(matrix1));
